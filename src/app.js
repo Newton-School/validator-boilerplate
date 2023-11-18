4861dc63db6ff2d15b75697f485b042c28ea4f39
MannShah17 <mannshah2004@gmail.com>
2023-11-18 18:52:34 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const validation = require('./routes/productRoutes');
+const validation = require('../routes/productRoutes');
 
 //middleware
 app.use(express.json());
