b36410d69eb11d89be8e85f7e961980b08c662ad
MannShah17 <mannshah2004@gmail.com>
2023-11-18 19:04:37 +0530

    done

--- models/productModel.js
+++ models/productModel.js
@@ -2,21 +2,10 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-  },
-  price: {
-    type: String,
-    required: [true, 'Price is required'],
-    validate: {
-      validator: function (value) {
-        // Custom price validation logic
-        return parseFloat(value) > 1000;
-      },
-      message: 'Price must be greater than 1000',
-    },
-  },
+  //write a code for product schema here
+  // Defining a Schema:
+  // name: name will be a string with required as true
+  // price: price will be a string with required as true. Also, write a custom validator to validate the price whose value is greater than 1000.
 });
 
 module.exports = mongoose.model('Validation', productSchema);
